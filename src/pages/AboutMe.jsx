a2a15025116107f693794302a2b64cd052b941e4
Omar <omar.m.2010masri@gmail.com>
2024-10-25 09:07:43 -0400

    illustration added. needs fixing

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+
 function AboutMe() {
   return (
-    <div class="container-border">
+    <div className="container-border">
       <h1>About Me</h1>
-      <p>This is the About Me page. You can add your personal information here.</p>
+      <div className="about-content">
+        <div className="about-text">
+          <p>This is the About Me page. You can add your personal information here.</p>
+        </div>
+        
+        {/* Video Section */}
+        <video autoPlay loop muted className="about-video">
+          <source src="/assets/Me.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
